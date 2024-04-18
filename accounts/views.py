8e7cfd1ae99ac7111c92d3e15b7318ddf072ded1
soyeongpark2090 <syp4546@gmail.com>
2024-04-18 14:00:52 +0900

    logout

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST, require_http_methods
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 
 @require_http_methods(['POST', 'GET'])
@@ -10,11 +11,32 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # request.uer은 로그인한 사용자 ->따라서 여기서는 쓸 수 없음
-            auth_login(request, form.get_user())
+            auth_login(request, user)
             return redirect('products:products_list')
     else:
         form = CustomUserCreationForm()
         context = {'form': form}
         return render(request, 'accounts/signup.html', context)
+
+
+@require_http_methods(['POST', 'GET'])
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('products:products_list')
+
+    else:
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return redirect('products:products_list')
