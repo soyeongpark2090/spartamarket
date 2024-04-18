1f76b8e5e4767637e27a5a74979bc0c1195dd6f2
soyeongpark2090 <syp4546@gmail.com>
2024-04-18 14:05:27 +0900

    회원탈퇴

--- accounts/views.py
+++ accounts/views.py
@@ -40,3 +40,11 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
         return redirect('products:products_list')
+
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+        return redirect('products:products_list')
